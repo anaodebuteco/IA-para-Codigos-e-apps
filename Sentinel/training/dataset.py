4dcb7364dd495781b39538c6e2630abaf0b86678
Pedro Silveira <pedrohenriquealves2000@yahoo.com>
2026-09-07 18:12:23 -0300

    separa exemplos no dataset da v3

--- Sentinel/training/dataset.py
+++ Sentinel/training/dataset.py
@@ -6,47 +6,64 @@ from tokenizer.tokenizer import Tokenizer
 
 
 class TextDataset(Dataset):
+
     def __init__(
         self,
         texts,
         tokenizer: Tokenizer,
         sequence_length: int,
     ):
+
         self.tokenizer = tokenizer
         self.sequence_length = sequence_length
 
-        token_ids = []
+        self.sequences = []
 
         for text in texts:
-            ids = tokenizer.encode(
+
+            token_ids = tokenizer.encode(
                 text,
                 add_bos=True,
                 add_eos=True,
             )
 
-            token_ids.extend(ids)
+            if len(token_ids) <= sequence_length:
+                continue
 
-        self.token_ids = torch.tensor(
-            token_ids,
-            dtype=torch.long,
-        )
+            for inicio in range(
+                0,
+                len(token_ids) - sequence_length,
+            ):
+
+                fim = inicio + sequence_length
+
+                input_ids = token_ids[
+                    inicio:fim
+                ]
+
+                target_ids = token_ids[
+                    inicio + 1:fim + 1
+                ]
+
+                self.sequences.append(
+                    (
+                        torch.tensor(
+                            input_ids,
+                            dtype=torch.long,
+                        ),
+                        torch.tensor(
+                            target_ids,
+                            dtype=torch.long,
+                        ),
+                    )
+                )
 
     def __len__(self):
-        return max(
-            0,
-            len(self.token_ids) - self.sequence_length,
-        )
+
+        return len(self.sequences)
 
     def __getitem__(self, index):
-        inicio = index
-        fim = index + self.sequence_length
 
-        input_ids = self.token_ids[
-            inicio:fim
-        ]
-
-        target_ids = self.token_ids[
-            inicio + 1:fim + 1
-        ]
+        input_ids, target_ids = self.sequences[index]
 
         return input_ids, target_ids
